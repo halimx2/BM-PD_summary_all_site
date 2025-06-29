0e79efe18cd52fdbfc918f3ef1fd9a3d75478d83
halimx2 <limchicken@naver.com>
2025-06-30 03:50:45 +0900

    update requirements.txt

--- load_data.py
+++ load_data.py
@@ -10,6 +10,10 @@ import re
 
 # load_dotenv()   # .env 읽어서 환경변수 등록
 # SHEET_ID  = os.getenv("SHEET_ID")
+import os
+
+SHEET_ID = os.environ["SHEET_ID"]
+
 SHEET_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:json"
 
 
