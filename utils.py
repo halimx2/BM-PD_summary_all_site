845ceb64ff60b6b22e252c0f9d0d86eaa7153921
halimx2 <limchicken@naver.com>
2025-07-15 01:29:21 +0900

    Multipages

---
+++ utils.py
@@ -0,0 +1,10 @@
+from options   import KIND_OPTIONS, SITE_OPTIONS, PROCESS_OPTIONS, UNIT_OPTIONS
+from load_data import load_sheet_data
+
+__all__ = [
+    "load_sheet_data",
+    "KIND_OPTIONS",
+    "SITE_OPTIONS",
+    "PROCESS_OPTIONS",
+    "UNIT_OPTIONS",
+]
